938e1c7eb4f0a0f6d481fa1310bfbf12003cd618
Naga Koushik <94678196+naga4121@users.noreply.github.com>
2022-02-16 12:27:12 +0000

    done

--- p1final.c
+++ p1final.c
@@ -1,22 +1,14 @@
-#include<stdio.h>
-void input(int*a,int*b)
+#include <stdio.h>
+void add()
 {
+  int a,b,c;
   printf("Enter two numbers\n");
-  scanf("%d%d",a,b);
-}
-void add(int a,int b,int*sum)
-{
-  *sum=a+b;
-}
-void output(int a,int b,int sum)
-{
-  printf("sum of %d+%d is %d\n",a,b,sum);
+  scanf("%d %d",&a,&b);
+  c=a+b;
+  printf("sum of %d+%d =%d\n",a,b,c);
 }
 int main()
 {
-  int a,b,sum;
-  input(&a,&b);
-  add(a,b,&sum);
-  output(a,b,sum);
+  add();
   return 0;
 }
